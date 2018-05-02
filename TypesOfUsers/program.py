31ed7c47b30194102fa0977bc29b6c75d29e9630
Arcadia <df.riveros11@uniandes.edu.co>
2018-05-02 11:40:22 -0500

    Creacion de ejemplo

---
+++ TypesOfUsers/program.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from DataBase.UserDB import User
+from DataBase.mongo_setup import global_init
+
+
+def main():
+    """Example"""
+    global_init('example')
+    u = User()
+    u.name = 'Diego'
+    u.email = 'df.riveros11@gmail.com'
+    u.save()
+
+
+if __name__ == '__main__':
+    main()
